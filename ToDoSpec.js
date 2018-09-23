bc150660f19375199bd90aa1bdeb5e7842e97fa4
Lisa <lsr488@gmail.com>
2018-09-23 16:08:29 -0400

    Pseudo checklist code

---
+++ ToDoSpec.js
@@ -0,0 +1,9 @@
+// functions to test
+// 	should add an item
+// 	should delete an item
+// 	should mark item as complete
+
+// dom manipulations to test
+// 	should inject initial html
+// 	should display new item
+// 	should trigger form and add item to todo array
